a721a0145cdd2962f7b88e13fb74202b960d7526
kleber.barilli <kleber.barilli73@gmail.com>
2022-04-04 16:18:20 -0300

    add github contrib

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,7 +2,8 @@ import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/auth';
 import './signin.css';
-import logo from '../../assets/logo2.png'
+import logo from '../../assets/logo2.png';
+import { FaGithub } from 'react-icons/fa';
 
 function SignIn() {
 	const [email, setEmail] = useState('');
@@ -22,9 +23,13 @@ function SignIn() {
 	  <div className="container-center">
 		<div className="login">
 		<h3>OnPlaces App</h3>
+	
 			<div className="login-area">
 				<img src={logo} alt="Logo" />
 			</div>
+			<a href="https://github.com/KleberBarilli/OnPlaces" target="_blank" rel="noreferrer" className="github">Contribua com o projeto no GitHub </a>
+
+			<a href="https://github.com/KleberBarilli/OnPlaces" target="_blank" rel="noreferrer"><FaGithub size={25}/></a>
 			<form onSubmit={handleSubmit}>
 				<h1>Entrar</h1>
 				<input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) } />
