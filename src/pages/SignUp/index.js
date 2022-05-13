46bea2bfdcf71b31cdae0bce2dff76b5b956d1cc
kleber.barilli <kleber.barilli73@gmail.com>
2022-05-13 12:42:43 -0300

    ajustes no cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/auth";
+import StarfieldAnimation from 'react-starfield-animation'
 import { FaGithub } from 'react-icons/fa';
 import logo from "../../assets/logo2.png";
 
@@ -21,6 +22,14 @@ function SignUp() {
 
   return (
     <div className="container-center">
+        <StarfieldAnimation
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          
+        }}
+      />
       <div className="login">
         <h3>OnPlaces App</h3>
         <div className="login-area">
@@ -44,6 +53,7 @@ function SignUp() {
           <FaGithub size={25} />
         </a>
         <form onSubmit={handleSubmit}>
+          <div className="campos">
           <h1>Cadastrar</h1>
           <input
             type="text"
@@ -62,7 +72,7 @@ function SignUp() {
             placeholder="*******"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-          />
+          /></div>
           <button type="submit">
             {loadingAuth ? "Carregando..." : "Cadastrar"}
           </button>
